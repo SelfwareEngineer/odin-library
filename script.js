a217f1469887c51208518f55099ca9a4a62ff6dc
SelfwareEngineer <126501922+SelfwareEngineer@users.noreply.github.com>
2024-07-31 15:05:21 -0400

    Rough UI pass; add logic for placeholder books to script.js

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 "use strict";
 
+// These are placeholders, disable when done styling
+const defaultBookCount = 12;
+const library = document.querySelector(".library");
+
+for (let i = 0; i < defaultBookCount; i++) {
+  const book = document.createElement("div");
+  book.classList.add("book");
+
+  const title = document.createElement("h3");
+  title.textContent = "Placeholder";
+  book.appendChild(title);
+
+  const author = document.createElement("h4");
+  author.textContent = "by Author";
+  book.appendChild(author);
+
+  const descHeader = document.createElement("h4");
+  descHeader.textContent = "Description:";
+  book.appendChild(descHeader);
+
+  const desc = document.createElement("p");
+  desc.textContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  book.appendChild(desc);
+
+  const status = document.createElement("h4");
+  status.textContent = "Status: not read";
+  book.appendChild(status);
+
+  library.appendChild(book);
+}
+
 const myLibrary = {};
 
 function Book(title, author, description, read) {
