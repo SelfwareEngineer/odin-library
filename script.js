1eae6c625468bfccf7ff5f9022d87343894e28f2
SelfwareEngineer <126501922+SelfwareEngineer@users.noreply.github.com>
2024-08-02 11:30:34 -0400

    Refactor to allow coloring of books via filters

--- script.js
+++ script.js
@@ -8,26 +8,30 @@ for (let i = 0; i < defaultBookCount; i++) {
   const book = document.createElement("div");
   book.classList.add("book");
 
+  const bookContent = document.createElement("div");
+  bookContent.classList.add("book-content");
+  book.appendChild(bookContent);
+
   const title = document.createElement("h3");
   title.textContent = "Placeholder";
-  book.appendChild(title);
+  bookContent.appendChild(title);
 
   const author = document.createElement("h4");
   author.textContent = "by Author";
-  book.appendChild(author);
+  bookContent.appendChild(author);
 
   const descHeader = document.createElement("h4");
   descHeader.textContent = "Description:";
-  book.appendChild(descHeader);
+  bookContent.appendChild(descHeader);
 
   const desc = document.createElement("p");
   desc.textContent =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-  book.appendChild(desc);
+  bookContent.appendChild(desc);
 
   const status = document.createElement("h4");
   status.textContent = "Status: not read";
-  book.appendChild(status);
+  bookContent.appendChild(status);
 
   library.appendChild(book);
 }
@@ -47,8 +51,6 @@ Book.prototype.toggleRead = function () {
 
 function addBookToLibrary(title, author, description, read) {
   myLibrary[title] = new Book(title, author, description, read);
-  const newBookIndex = myLibrary.length - 1;
-  return newBookIndex;
 }
 
 function updateLibrary() {
@@ -59,7 +61,7 @@ function updateLibrary() {
   }
 }
 
-function removeBook(name) {
+function removeBook(id) {
   // remove book of name from myLibrary
 
   updateLibrary();
